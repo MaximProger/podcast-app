d835d19d3a6dae33d738e4bc4d2c1df13f728d5f
Maxim Koshman <maksimkoshman090@gmail.com>
2020-01-15 14:25:09 +0300

    add list rendering function

--- src/app.js
+++ src/app.js
@@ -1,1 +1,34 @@
+import {Question} from './question'
+import {isValid} from './utils'
 import './style.css'
+
+const form = document.getElementById('form');
+const input = form.querySelector('#question__input');
+const sumbitBtn = form.querySelector('#submit');
+
+window.addEventListener('load', Question.renderList)
+form.addEventListener('submit', submitFormHandler)
+input.addEventListener('input', () => {
+    sumbitBtn.disabled = !isValid(input.value)
+})
+
+function submitFormHandler(event) {
+    event.preventDefault()
+
+    if (isValid(input.value)) {
+        const question = {
+            text: input.value.trim(),
+            date: new Date().toJSON()
+        }
+        sumbitBtn.disabled = true
+        // Async request to server
+        Question.create(question).then(() => {
+            input.value = ''
+            input.className = ''
+            sumbitBtn.disabled = false
+        })
+
+    }
+
+    console.log(input.value)
+}
